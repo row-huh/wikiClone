0efd79a3aa76f98aadc82acc37e373b09f54856c
Roha Pathan <rohaabropathan@gmail.com>
2023-07-26 22:31:04 +0500

    created new videw - loadEntry that loads an entry given in url bar

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,4 +7,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+    
+# whenever someone adds a title at /wiki/title, will load an html related to that entry
+def loadEntry(request, title):
+    if util.get_entry(title):
+        ...
+    else:
+        ...
