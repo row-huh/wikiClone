8cef953f784268c17293ecd443d8a4d2470b53a0
Roha Pathan <RohaAbroPathan@gmail.com>
2023-08-24 16:24:44 -0400

    .

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wiki", views.index, name="index"),
+    path("wiki/", views.index, name="index"),
     path("wiki/<str:title>", views.loadEntry, name="title"),
-    path("wiki/?q=<str:entry>", views.searchEntry, name="search")
+    path("wiki/search", views.searchEntry, name="search")
 ]
