5c98f20d0c7135ba0694c7b2d669eba4c9994fa5
Roha Pathan <RohaAbroPathan@gmail.com>
2023-08-26 17:22:23 -0400

    added new path for random page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/search", views.searchEntry, name="search"),
     path("wiki/create", views.create, name="create"),
     path("wiki/createentry", views.createEntry, name="createentry"),
+    path("wiki/random", views.random, name="random"),
     path("wiki/<str:title>", views.loadEntry, name="title"),
 ]
